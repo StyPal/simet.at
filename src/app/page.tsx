a4ddd69cc5192672a2eda7ad7bc1de70a0eaa863
StyPal <126196078+StyPal@users.noreply.github.com>
2024-11-08 11:57:59 +0100

    music button dark mode color updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <Link
-                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-blue dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-blue text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
                     href="/music"
                     rel="noopener noreferrer"
                 >
