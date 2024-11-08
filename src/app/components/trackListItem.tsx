1c8b76133d359aebcf25d67e1dc0f50d3b7aaa01
StyPal <126196078+StyPal@users.noreply.github.com>
2024-11-08 23:47:16 +0100

    Audios added

---
+++ src/app/components/trackListItem.tsx
@@ -0,0 +1,25 @@
+// components/AudioTrackItem.tsx
+import React from "react";
+
+interface AudioTrackItemProps {
+    file: string;
+    isSelected: boolean;
+    duration: string;
+    onSelect: () => void;
+}
+
+const AudioTrackItem: React.FC<AudioTrackItemProps> = ({ file, isSelected, duration, onSelect }) => {
+    return (
+        <li
+            className={`flex justify-between items-center w-full mb-4 p-2 cursor-pointer rounded text-background ${
+                isSelected ? ' bg-gray-light dark:bg-gray-dark font-bold' : 'bg-gray dark:bg-gray'
+            }`}
+            onClick={onSelect}
+        >
+            <span>{file.split('/').pop()}</span> {/* Track name */}
+            <span className="text-sm">{duration}</span>
+        </li>
+    );
+};
+
+export default AudioTrackItem;
