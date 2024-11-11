00e7a2361839a461275e0ce1a489d928ccec8858
StyPal <126196078+StyPal@users.noreply.github.com>
2024-11-11 10:39:49 +0100

    music link deleted on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import {Header} from "@/app/components/header";
 import {SocialMediaFooter} from "@/app/components/footer";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
     return (
@@ -11,17 +9,7 @@ export default function Home() {
             <Header/>
 
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <Link
-                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-blue text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                    href="/music"
-                    rel="noopener noreferrer"
-                >
-                    <Image src="/8thNote.png" alt="" width={16}
-                           height={16} className="invert-svg"/>
-                    <label>
-                        Go to Music →
-                    </label>
-                </Link>
+
             </main>
 
             <SocialMediaFooter/>
